5c19c4eda113a75687f661d6707ea4868e10c465
Виталик <perry_06@mail.ru>
2022-10-20 13:53:29 +0300

    Vitali_Zalutski_homework21 corrected

--- homework/Vitali_Zalutski/Home_Work_21/test_Vitali_Zalutski_hw21.py
+++ homework/Vitali_Zalutski/Home_Work_21/test_Vitali_Zalutski_hw21.py
@@ -1,45 +1,65 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from time import sleep
-
-service = Service(executable_path='C:\\chromedriver.exe')
 
 
 @pytest.fixture(scope='session')
-def driver():
-    print('Before test')
-    options = Options()
-    options.add_argument('start-maximized')
-    chrome_driver = webdriver.Chrome(options=options, service=service)
-    sleep(5)
-    yield chrome_driver
-    chrome_driver.quit()
-    print(' code off')
+def testing():
+    print(f'{"-" * 5} Before all test! {"-" * 5}')
+    yield None
+    print(f'{"-" * 5} After all test! {"-" * 5}')
+
+
+@pytest.fixture(scope='function')
+def test_one():
+    print(f'{"-" * 5} Before test! {"-" * 5}')
+    yield None
+    print(f'{"-" * 5} After test! {"-" * 5}')
 
 
 @pytest.mark.simple
-def test_simple_1():
-    print('Before Testing "test_simple_1"')
+def test_simple_1(testing, test_one):
+    print('testing_simple_1')
     assert 5 == 5
-    print('After Testing "test_simple_1"')
+
+
+@pytest.mark.simple
+def test_simple_2(test_one):
+    print('testing_simple_2')
+    assert 6 == 6
 
 
 @pytest.mark.hard
-def test_go_to_site(driver):
-    print('Testing button "contact_us"')
-    driver.get('http://automationpractice.com/index.php')
-    element_button = driver.find_element(By.ID, 'contact-link')
-    element_button.click()
-    sleep(3)
-    print('After Testing')
+def test_hard_1(test_one):
+    print('testing_hard_1')
+    assert 1 == 1
 
 
-@pytest.mark.skip("this code is incomplete")
-def test_simple_2():
-    print('Before Testing "test_simple_2"')
-    assert 15 != 10
+@pytest.mark.hard
+def test_hard_2(test_one):
+    print('testing_hard_2')
+    assert 2 == 2
 
 
+@pytest.mark.skip('This test not working')
+def test_skip(test_one):
+    print('testing_skip')
+    assert 3 == 3
+
+
+TEST_DATA = [2, 3, 4]
+
+
+@pytest.mark.parametrize(
+    'num',
+    TEST_DATA
+)
+
+
+class TestingParametrize:
+    def test_one(self, num):
+        assert num == 2
+
+    def test_two(self, num):
+        assert num == 3
+
+    def test_three(self, num):
+        assert num == num
