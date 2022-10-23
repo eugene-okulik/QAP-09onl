d22211897dc9688c3855d5094d6d156fe3a999f6
xSmugxJoex <xsmugxjoex@yandex.ru>
2022-10-23 12:27:19 +0300

    HomeWork_20 full complited

--- homework/Denis_Fadeev/homework_20/Task_1_additional.py
+++ homework/Denis_Fadeev/homework_20/Task_1_additional.py
@@ -14,16 +14,22 @@ def choose_contact_us(chrome_driver):
     chrome_driver.get('http://automationpractice.com/')
     contact_us_button = chrome_driver.find_element(By.ID, 'contact-link')
     contact_us_button.click()
+
     write_email = chrome_driver.find_element(By.CSS_SELECTOR, 'input[data-validate="isEmail"]')
     write_email.send_keys('my@mail.ru')
+
     write_order_reference = chrome_driver.find_element(By.ID, 'id_order')
     write_order_reference.send_keys('qwerty')
+
     write_texts = chrome_driver.find_element(By.ID, 'message')
     write_texts.send_keys('TEXT')
+
+    to_add_a_file = chrome_driver.find_element(By.ID, 'fileUpload')
+    to_add_a_file.send_keys('C:\\Users\\I_h8you\\Desktop\\QAP-09onl\\QAP-09onl\\homework\\Denis_Fadeev\\homework_20\\original.jpg')
+
     send_button = chrome_driver.find_element(By.ID, 'submitMessage')
     send_button.click()
     sleep(5)
-
 
 chrome_driver = open_chrome()
 choose_contact_us(chrome_driver)
