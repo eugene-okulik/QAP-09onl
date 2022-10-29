ddac89b568aadd2e66cd484a83d5ca9736c2c444
AmelchenkoDima <amelchencko.dima1998@gmail.com>
2022-10-29 18:46:47 +0300

    fixed

--- homework/Amelchenko_Dima/hw_22/hw_22.py
+++ homework/Amelchenko_Dima/hw_22/hw_22.py
@@ -1,21 +1,24 @@
+import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
+@pytest.fixture(scope='session')
 def driver():
     options = Options()
     options.add_argument('start-maximized')
     driver_chrome = webdriver.Chrome(options=options)
     driver_chrome.implicitly_wait(10)
-    return driver_chrome
+    yield driver_chrome
+    driver_chrome.quit()
 
 
-def task1(driver):
+def test_task1(driver):
     driver.get('https://www.demoblaze.com/index.html')
 
     samsung_galaxy_s6 = driver.find_element(By.CSS_SELECTOR, 'a[href="prod.html?idp_=1"]')
@@ -38,13 +41,11 @@ def task1(driver):
     open_cart.click()
 
     check_cart = driver.find_element(By.XPATH, '//*[@id="tbodyid"]/tr/td[2]')
-    driver.implicitly_wait(10)
     assert check_cart.text == 'Samsung galaxy s6'
     print('Device "Samsung galaxy s6" added to cart')
-    driver.quit()
 
 
-def task2(driver):
+def test_task2(driver):
     driver.get('https://demoqa.com/menu#')
 
     main_item_2 = driver.find_element(By.XPATH, '//*[@id="nav"]/li[2]/a')
@@ -56,10 +57,8 @@ def task2(driver):
     sud_sub_item_2 = driver.find_element(By.XPATH, '//*[@id="nav"]/li[2]/ul/li[3]/ul/li[2]/a')
     sud_sub_item_2.click()
 
-    driver.quit()
 
-
-def task3(driver):
+def test_task3(driver):
     driver.get('https://testpages.herokuapp.com/styled/alerts/alert-test.html')
 
     text = 'Funny ducks'
@@ -74,9 +73,9 @@ def task3(driver):
 
     assert check_text == text
 
-
-driver = driver()
-task1(driver)
-task2(driver)
-task3(driver)
-driver.quit()
+#
+#driver = driver()
+#test_task1(driver)
+#test_task2(driver)
+#test_task3(driver)
+#
