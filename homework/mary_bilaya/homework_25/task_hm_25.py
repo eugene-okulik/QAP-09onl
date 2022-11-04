3d18047f6154b23ad4b0a19e35e451e71b07c400
Mary Bilaya <marybilaya@gmail.com>
2022-11-04 18:50:26 +0300

    homework_25_changed_result_in_one_line

--- homework/mary_bilaya/homework_25/task_hm_25.py
+++ homework/mary_bilaya/homework_25/task_hm_25.py
@@ -14,36 +14,22 @@ class PersonalData:
         with open(self.user_file, 'r') as data_file:
             return json.load(data_file)
 
-    def name_info(self):
-        return f'The users name is {self.name}. '
-
-    def city_info(self):
-        return f'He has lived in {self.city}. '
-
-    def children_info(self):
-        return f'He has {len(self.children)} children. Their names are {", ".join(self.children)}.'
-
-    def has_info(self):
-        print('He owns: ', end="")
+    def user_info(self):
+        print(f'The users name is {self.name}. '
+              f'He has lived in {self.city}. '
+              f'He has {len(self.children)} children. Their names are {", ".join(self.children)}. '
+              f'He owns: ')
         for key, value in self.has.items():
-            print(f'{key} {value}', end=", ")
+            print(f'{key}{value}', end=", ")
 
 
 user_1 = PersonalData('user1.json')
-print(user_1.name_info())
-print(user_1.city_info())
-print(user_1.children_info())
-print(user_1.has_info())
+print(user_1.user_info())
 print('\n')
 user_2 = PersonalData('user2.json')
-print(user_2.name_info())
-print(user_2.city_info())
-print(user_2.children_info())
-print(user_2.has_info())
+print(user_2.user_info())
 print('\n')
 user_3 = PersonalData('user3.json')
-print(user_3.name_info())
-print(user_3.city_info())
-print(user_3.children_info())
-print(user_3.has_info())
+print(user_3.user_info())
+
 
