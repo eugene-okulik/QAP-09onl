8308c4051fa6d695157cccdee2855273a4c31c55
Malashko17 <109578401+Malashko17@users.noreply.github.com>
2022-11-07 20:26:59 +0300

    Update test_requests.py

--- homework/Pavel_Malashko/Homework_26/test_requests.py
+++ homework/Pavel_Malashko/Homework_26/test_requests.py
@@ -24,13 +24,14 @@ def test_create_new_meme(base_url, user_authorization):
     assert response['url'] == "https://sun9-35.userapi.com/impg/aQnVdpGvRGcsisBvX3k_uJ2mz04qMZ-Wi8vFaw/9bPt8FYiHl8.jpg?size=1028x1110&quality=95&sign=66dceafcc6ef0b6aff613655a52beed5&type=album"
 
 
-def test_update_meme(base_url, user_authorization):
+def test_update_meme(base_url, user_authorization, create_a_meme):
+    meme_id = create_a_meme
     headers = {
         "Authorization": f'{user_authorization}',
         'Content-Type': 'application/json'
     }
     data_json = {
-        "id": 2,
+        "id": meme_id,
         "text": "funny meme",
         "url": "https://sun9-3.userapi.com/impg/MhZfx8fqO9BOQLx2LQU3uTpeWPzFL8dHkKKgQA/8gGJdqbZ9Ow.jpg?size=827x1029&quality=96&sign=c61d4cf5bac48e8659cb6b586bdffbcb&type=album",
         "tags": ["old man", "technical support", "hamster", "mouse", "fun"],
@@ -40,26 +41,27 @@ def test_update_meme(base_url, user_authorization):
     data = json.dumps(data_json)
     response = requests.request(
         'PUT',
-        f'{base_url}/meme/2',
+        f'{base_url}/meme/{meme_id}',
         headers=headers,
         data=data
     ).json()
     assert response['url'] == "https://sun9-3.userapi.com/impg/MhZfx8fqO9BOQLx2LQU3uTpeWPzFL8dHkKKgQA/8gGJdqbZ9Ow.jpg?size=827x1029&quality=96&sign=c61d4cf5bac48e8659cb6b586bdffbcb&type=album"
 
 
-def test_delete_post(base_url, user_authorization):
+def test_delete_post(base_url, user_authorization, create_a_meme):
+    meme_id = create_a_meme
     headers = {
         "Authorization": f'{user_authorization}',
         'Content-Type': 'application/json'
     }
     requests.request(
         'DELETE',
-        f'{base_url}/meme/2',
+        f'{base_url}/meme/{meme_id}',
         headers=headers
     )
     response = requests.request(
         'GET',
-        f'{base_url}/meme/2',
+        f'{base_url}/meme/{meme_id}',
         headers=headers
     )
     assert response.status_code == 404, 'Deleted meme is not deleted'
@@ -74,6 +76,7 @@ def test_get_all_meme(base_url, user_authorization):
         f'{base_url}/meme',
         headers=headers
     ).json()
-    for meme in range(len(response['data'])):
-        tags = (response['data'][meme])
-        assert 'fun' in tags['tags'], 'tag fun is not found'
+    for memes in response['data']:
+        for tag in memes['tags']:
+            if tag == 'fun':
+                assert tag == 'fun', 'meme with "fun" tag is not found'
