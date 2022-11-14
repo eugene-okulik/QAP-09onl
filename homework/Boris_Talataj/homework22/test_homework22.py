ce81024c288de9a579d2e9f9487b44e0b0cac0fd
Boris Talataj <boristalataj@gmail.com>
2022-11-14 20:20:06 +0300

    fixed_homework22

--- homework/Boris_Talataj/homework22/test_homework22.py
+++ homework/Boris_Talataj/homework22/test_homework22.py
@@ -1,33 +1,30 @@
-from selenium import webdriver
 from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
-import pytest
 
 
 def test1(driver):
     driver.get('https://www.demoblaze.com/')
-    sleep(2)
+    WebDriverWait(driver, 1)
     lumia = driver.find_element(By.XPATH, '//*[@id="tbodyid"]/div[2]/div/div/h4/a')
     ActionChains(driver).key_down(Keys.CONTROL).click(lumia).key_up(Keys.CONTROL).perform()
     tab = driver.window_handles
     driver.switch_to.window(tab[1])
-    sleep(2)
+    sleep(1)
     name = driver.find_element(By.XPATH, "//*[contains(@class,'name')]").text
     print(f" chose: {name}")
-    add_button = driver.find_element(By.XPATH, '//*[@id="tbodyid"]/div[2]/div/a')
+    add_button = driver.find_element(By.LINK_TEXT, 'Add to cart')
     add_button.click()
-    sleep(5)
+    sleep(1)
     Alert(driver).accept()
     driver.close()
     driver.switch_to.window(tab[0])
     cart_button = driver.find_element(By.XPATH, '//*[@id="cartur"]')
     cart_button.click()
-    sleep(2)
-    product_name = driver.find_element(By.XPATH, '//*[@id="tbodyid"]/tr/td[2]')
+    sleep(1)
     name1 = driver.find_element(By.XPATH, '//*[@id="tbodyid"]/tr/td[2]').text
     print(f"Product in cart: {name1}")
     assert name == name1
@@ -43,7 +40,6 @@ def test2(driver):
     sub_sub_list_buttons = sub_sub_list.find_elements(By.TAG_NAME, 'li')
     sub_sub_item_2 = sub_sub_list_buttons[1]
     ActionChains(driver).move_to_element(main_item_2).move_to_element(sub_sub_list).click(sub_sub_item_2).perform()
-    sleep(0.5)
 
 
 def test3(driver):
@@ -53,7 +49,7 @@ def test3(driver):
     my_text = ('Something')
     Alert(driver).send_keys(my_text)
     Alert(driver).accept()
-    sleep(5)
+    WebDriverWait(driver, 1)
     alert_text = driver.find_element(By.ID, 'promptreturn').text
     print(f'\nYou asked me to write: {alert_text}')
     assert my_text == alert_text
