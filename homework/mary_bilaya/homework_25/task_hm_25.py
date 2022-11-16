72551ad9b76f0bfed84819a1d5b67e961b3d5442
Mary Bilaya <marybilaya@gmail.com>
2022-11-16 23:06:13 +0300

    homework_25_latest_correction

--- homework/mary_bilaya/homework_25/task_hm_25.py
+++ homework/mary_bilaya/homework_25/task_hm_25.py
@@ -18,9 +18,9 @@ class PersonalData:
         print(f'The users name is {self.name}. '
               f'He has lived in {self.city}. '
               f'He has {len(self.children)} children. Their names are {", ".join(self.children)}. '
-              f'He owns: ')
-        for key, value in self.has.items():
-            print(f'{key}{value}', end=", ")
+              f'He owns: ', end="")
+        for k, v in self.has.items():
+            print(", ".join([f'{k} - {v}']), end=", ")
 
 
 user_1 = PersonalData('user1.json')
